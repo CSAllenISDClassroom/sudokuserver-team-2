ea257a3e33ec12860127ec71c8fc6ed52152bf5d
Reese Hoffart <reese.hoffart@student.allenisd.org>
2021-09-14 10:23:01 -0500

    initial commit

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -8,4 +8,8 @@ func routes(_ app: Application) throws {
     app.get("hello") { req -> String in
         return "Hello, world!"
     }
+
+    app.post("games") { req -> String in
+        return "no"
+    }
 }
