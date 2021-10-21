ee50501152ab3dc00e3a39b592c92bd706900a34
Reese Hoffart <reese.hoffart@student.allenisd.org>
2021-10-21 17:17:53 -0500

    added badRequest error reasons

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -12,7 +12,7 @@ func routes(_ app: Application) throws {
         guard let inputDifficulty = inputDifficulty,
               inputDifficulty == "easy" || inputDifficulty == "medium" || inputDifficulty == "hard" || inputDifficulty == "hell",
               let setDifficulty = toDifficulty(inputDifficulty:inputDifficulty) else{
-            return Response(status:.badRequest)
+            throw Abort(.badRequest, reason:"difficulty specified doesn't match requirements")
         }
         print("[\(games.count)] Difficulty:\(setDifficulty). ", terminator:"")
         games.append(Board(difficulty:setDifficulty))
@@ -28,11 +28,13 @@ func routes(_ app: Application) throws {
         let inputFilter : String? = req.query["filter"]
         guard let inputFilter = inputFilter,
               inputFilter == "all" || inputFilter == "repeated" || inputFilter == "incorrect",
-              let setFilter = toFilter(inputFilter:inputFilter),
-              let id : Int = req.parameters.get("id"),
-              id < games.count && id >= 0
+              let setFilter = toFilter(inputFilter:inputFilter)
         else {
-            return Response(status:HTTPResponseStatus.badRequest)
+            throw Abort(.badRequest, reason:"filter specified doesn't match requirements")
+        }
+        guard let id : Int = req.parameters.get("id"),
+              id < games.count && id >= 0 else {
+            throw Abort(.badRequest, reason:"id specified doesn't match requirements")
         }
         print("[\(id)] Client is requsting board state.")
         let body = games[id].toJSON(filter:setFilter)
@@ -45,20 +47,24 @@ func routes(_ app: Application) throws {
 
     app.put("games", ":id", "cells", ":boxIndex", ":cellIndex") { req -> Response in
         guard let id : Int = req.parameters.get("id"),
-              let boxIndex : Int = req.parameters.get("boxIndex"),
-              let cellIndex : Int = req.parameters.get("cellIndex"),
-              id < games.count && id >= 0,
-              boxIndex >= 0 && boxIndex < 9,
+              id < games.count && id >= 0 else {
+            throw Abort(.badRequest, reason:"id specified doesn't match requirements")
+        }
+        guard let boxIndex : Int = req.parameters.get("boxIndex"),
+              boxIndex >= 0 && boxIndex < 9 else {
+            throw Abort(.badRequest, reason:"boxIndex is out of range 0 ... 8")
+        }
+        guard let cellIndex : Int = req.parameters.get("cellIndex"),
               cellIndex >= 0 && cellIndex < 9
         else {
-            return Response(status:HTTPResponseStatus.badRequest)
+            throw Abort(.badRequest, reason:"cellIndex is out of range 0 ... 8")
         }
         var input : Int? = nil
         let pos = BCtoXY(b:boxIndex, c:cellIndex)
         var validMove : Bool = games[id].removeNumber(xPos:pos.0, yPos:pos.1)
         if let inputValue = try req.content.decode(InputValue.self).value {
             guard inputValue >= 1 && inputValue <= 9 else {
-                return Response(status:.badRequest)
+                throw Abort(.badRequest, reason: "value is out of range 1 ... 9 or null")
             }
             validMove = games[id].insertNumber(xPos:pos.0, yPos:pos.1, number:inputValue)
             input = inputValue
